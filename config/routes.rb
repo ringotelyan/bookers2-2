17160ebd014660cb86cbfe89eb298c29ba3f6679
ringotelyan <sute.dh9999@gmail.com>
2022-09-05 02:16:07 +0000

    [Update]controllerファイルの作成users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
   devise_for :users
   root to: "homes#top"
   get '/about' => "homes#about"
-  resouces :books, onlu: [:index, :show, :edit, :update]
+  resources :books, only: [:index, :show, :edit, :update, :create]
+  resources :users, only: [:index, :show, :edit, :update]
 end
